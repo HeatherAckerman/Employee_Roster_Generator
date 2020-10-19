d16fc3b47e895dde67d14f24340cc1ef6ab79b8f
Heather Ackerman <hla20115213@gmail.com>
2020-10-19 09:55:44 -0500

    Wrote Employee.js to pass all tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -3,5 +3,29 @@
 //REMEMBER: CREATE IT TO PASS ALL TESTS
 
 // Create employee class with a constructor include name, id, and email
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    };
+
+    getName() {
+        return this.name;
+    };
+
+    getId() {
+        return this.id;
+    };
+
+    getEmail() {
+        return this.email;
+    };
+
+    getRole() {
+        return "Employee"
+    };
+};
 
 // Make it a module.exports
+module.exports = Employee;
